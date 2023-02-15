c105506696f56d7611e6a9789461071bb5893ce5
João Abreu <jvabreusousa12@gmail.com>
2023-02-15 17:21:24 -0300

    Feat: Adding Header and cofiguring Routes within it

---
+++ src/layout/Header/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "./Header.module.scss";
+import { MdOutlineLogin } from "react-icons/md";
+import logoMain from "../../../public/images/logo_sollaris.png"
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__logo}>
+        <h1><img src={logoMain.src} alt="Logo Sollaris" /></h1>
+      </div>
+      <nav>
+        <Link href="/#" className={styles.header__btn_login}>
+          <MdOutlineLogin />
+          <p>Log-In</p>
+        </Link>
+        <Link href="/cadastroUsuario" className={styles.header__btn_cadastro}>
+          <p>Cadastre-se</p>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+export default Header;
