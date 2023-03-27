b0ff28145cc70b1af4392072a0ab1e4f3e7755be
João Abreu <jvabreusousa12@gmail.com>
2023-03-27 17:43:51 -0300

    Fix: Merge

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import styles from './SearchBar.module.scss';
 
 const SearchBar = () => {
@@ -12,18 +12,3 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
-=======
-import styles from './SearchBar.module.scss'
-
-const SearchBar = () => {
-    return (
-            <input
-                type="search"
-                placeholder='Buscar produtos'
-                className={styles.searchbar}
-            />
-    )
-}
-
-export default SearchBar;
->>>>>>> f7d09598f307d735ade67fcab199206af078adb7
