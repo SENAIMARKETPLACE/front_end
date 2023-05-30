5c1813b7e9cc36f3adcfbe921033a751f6d46b6f
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-05-30 16:24:02 -0300

    feat: add new images in carousel banner

--- src/components/Carousel/BannerCarousel/index.tsx
+++ src/components/Carousel/BannerCarousel/index.tsx
@@ -4,6 +4,7 @@ import banner1 from '../../../../public/images/banner/banner01.jpg';
 import banner2 from '../../../../public/images/banner/banner02.jpg';
 import banner3 from '../../../../public/images/banner/banner03.jpg';
 import banner4 from '../../../../public/images/banner/banner04.jpg';
+import banner5 from '../../../../public/images/banner/banner05.jpg';
 import Link from 'next/link';
 
 const images = [
@@ -49,6 +50,9 @@ export default function BannerCarousel() {
       </Carousel.Slide>
       <Carousel.Slide className={styles.carousel__slide}>
         <img src={banner4.src} alt={''} />
+      </Carousel.Slide>
+      <Carousel.Slide className={styles.carousel__slide}>
+        <img src={banner5.src} alt={''} />
       </Carousel.Slide>
     </Carousel>
   );
