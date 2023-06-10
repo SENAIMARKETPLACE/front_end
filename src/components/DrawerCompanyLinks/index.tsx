40636f2fc5ee27ffcc63825bb0cc9278be32a0a2
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-06-10 17:39:02 -0300

    feat: add responsive menu small screens

---
+++ src/components/DrawerCompanyLinks/index.tsx
@@ -0,0 +1,60 @@
+import { IconBuildingStore, IconTag } from '@tabler/icons-react';
+import styles from './DrawerCompanyLinks.module.scss';
+import { Burger, Center, Drawer } from '@mantine/core';
+import MiniSearchBar from 'components/MiniSearchBar';
+import Link from 'next/link';
+import LogoSollaris from '../../../public/images/Logo.svg';
+import { useDisclosure } from '@mantine/hooks';
+import SearchBar from 'components/SearchBar';
+
+export function DrawerCompanyLinks() {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const [openedBurguer, { toggle }] = useDisclosure(false);
+  const label = opened ? 'Close navigation' : 'Open navigation';
+
+  const mockdata = [
+    { icon: IconTag, label: 'Meus Produtos', path: '/empresa/produtos' },
+    {
+      icon: IconBuildingStore,
+      label: 'Minha Loja',
+      path: '/empresa/minha-loja',
+    },
+  ];
+
+  return (
+    <>
+      <Drawer
+        size="320px"
+        opened={opened}
+        onClose={close}
+        className={styles.drawer}
+      >
+        <Center>
+          <Link href={'/marketplace'}>
+            <img src={LogoSollaris.src} alt="Logo do Sollaris" />
+          </Link>
+        </Center>
+        <Center mt={40}>
+          <SearchBar />
+        </Center>
+
+        <ul className={styles.drawer__list}>
+          {mockdata.map((link) => (
+            <Link href={link.path} key={link.label}>
+              <li key={link.label}>{link.label}</li>
+            </Link>
+          ))}
+        </ul>
+      </Drawer>
+
+      <Burger
+        opened={opened}
+        onClick={open}
+        aria-label={label}
+        size={'md'}
+        color="#5f78e7"
+      />
+    </>
+  );
+}
--- src/layout/LayoutBusiness/index.tsx
+++ src/layout/LayoutBusiness/index.tsx
@@ -7,6 +7,7 @@ import { AppShell, Navbar, Header } from '@mantine/core';
 import { Sidebar } from 'layout/Sidebar';
 import EmpresaBanner from 'components/EmpresaBanner';
 import Banner from '../../../public/images/banner.png';
+import { DrawerCompanyLinks } from 'components/DrawerCompanyLinks';
 
 interface LayoutBusinessProps {
   children: ReactNode;
@@ -16,12 +17,30 @@ interface LayoutBusinessProps {
 const LayoutBusiness = ({ children, quantidade }: LayoutBusinessProps) => {
   const [isLogged, setIsLogged] = useState(true);
 
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    setWindowWidth(window.innerWidth);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <div className={styles.layout}>
         <Sidebar />
         <div className={styles.layout__content}>
           <EmpresaBanner image={Banner} alt="Capa da empresa" />
+          <div className={styles['layout__content--menu']}>
+            {windowWidth < 850 && <DrawerCompanyLinks />}
+          </div>
           <section className={styles['layout__content--children']}>
             {children}
           </section>
